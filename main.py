3d83a7f2b1e2aa01f134d7cd743bceb173dff852
RyanCheng98153 <ryan98153@gmail.com>
2024-05-08 02:22:57 +0800

    add Bond Examples

--- main.py
+++ main.py
@@ -5,35 +5,42 @@ from graph import *
 import utils
 from solvers import Solver
 
-def main( Length:int, Height:int ):
+def main( Length:int, Height:int=1, JLayer = 1.0, JHeight = 1.0 ):
     H = dict()
     J = dict()
-    
-    JLayer = 1.0
-    JHeight = 1.0
     
     for i in range(0, L):
         for j in range(0, L):
             # x, y = toCoordinate()
             index = toIndex(Length, i, j)
             H[index] = 0
-    
-    for i in range(0, L):
-        for j in range(0, L):
-            # x, y = toCoordinate()
-            index = toIndex(Length, i, j )
-            right = getRight(Length, index )
-            bottom = getBottom(Length, index)
-            bottomRight = getBottomRight(Length, index )
             
-            J[(index, right)] = 1
-            J[(index, bottom)] = 1
-            J[(index, bottomRight)] = 1
+    def fullyconnect(J):
+        for i in range(0, L):
+            for j in range(0, L):
+                # x, y = toCoordinate()
+                index = toIndex(Length, i, j )
+                right = getRight(Length, index )
+                bottom = getBottom(Length, index)
+                bottomRight = getBottomRight(Length, index )
+                
+                J[(index, right)] = JLayer
+                J[(index, bottom)] = JLayer
+                J[(index, bottomRight)] = JLayer
+        return J
+    
+    # J = fullyconnect(J)
+    J = customBond(J)
             
     solver = Solver(Length, Height, JLayer, JHeight)
     solver.doExactSolver(H, J)
     solver.printIsing(None, J)
 
+def customBond( J:dict ):
+    print("insert the bonds")
+    
+    
+    return J
 
 def test(status:bool):
     # for testing
