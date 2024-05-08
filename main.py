5431e3ec965e167368de463e08d3119cd8c3a522
RyanCheng98153 <ryan98153@gmail.com>
2024-05-08 18:54:11 +0800

    feat:json reader

--- main.py
+++ main.py
@@ -7,26 +7,74 @@ import json
 def getJson(file:str):
     #with open("9_9_1_gamma0_2.json") as f:
     with open(file) as f:
-        data = f.readlines()
+        data = str()
+        for line in f.readlines():
+            data += line
+            
         return json.loads(data)
+        
+        # data = f.readlines()
+        # return json.loads(data)
+
+def printJdataInfo(jdata):
+    model_info = jdata["model_info"]
+    Length = model_info["length"]
+    Height = model_info["height"]
+    
+    print("\n[model_info information]:")
+    print(f"Length: {Length}, {Length + 1}")
+    print(f"Height: {Height}, {Height + 1}")
+    
+    print("\n[node information]:")
+    node = jdata["nodes"][0]
+    id = node["id"]
+    print(f"nodes: {id}, nodes+1: {id+1}")
+    magnetic_field = node["magnetic_field"]
+    print(f"magnetic_field: {magnetic_field}, magnetic_field+1: {magnetic_field+1}")
+    
+    print("\n[bond information]:")
+    bond = jdata["bonds"][0]
+    srcNode = bond["node_ids"][0]
+    print(f"src_index: {srcNode}, src_index+1: {srcNode+1}")
+    tgtNode = bond["node_ids"][1]
+    print(f"tgt_index: {tgtNode}, tgt_index+1: {tgtNode}")
+    strength = bond["strength"]
+    print(f"strength: {strength}, strength+1: {strength+1}")
 
 # def main( Length:int, Height:int ):
-def main(Length, Height = 1):
+def main():
     H = dict()
     J = dict()
     
-    J_layer = 1.0
-    J_height = 1.0
+    jdata = getJson(sys.argv[1])
+    
+    
+    model_info = jdata["model_info"]
+    Length = model_info["length"]
+    Height = model_info["height"]
+    
+    # printJdataInfo(jdata)
+    
     solver = Solver(Length, Height)
-    H, J = solver.fullyConnect( J_layer, J_height=1.0)
-    # H, J = solver.customConnect()
     
-    # jdata = getJson(sys.argv[1])
-    # solver = Solver()
+    for node in jdata["nodes"]:
+        index = node["id"]
+        magnetic_field = node["magnetic_field"]
+        H[index] = magnetic_field
     
+    for bond in jdata["bonds"]:
+        srcNode = bond["node_ids"][0]
+        tgtNode = bond["node_ids"][1]
+        strength = bond["strength"]
+        
+        J[(srcNode, tgtNode)] = strength
     
-    solver.doExactSolver(H, J)
-    solver.printIsing(H, J)
+    # print(H)
+    # print(J)
+    
+    #solver = Solver()
+    #solver.doExactSolver(H, J)
+    #solver.printIsing(H, J)
 
 
 def test(status:bool):
@@ -45,18 +93,4 @@ if __name__ == "__main__":
         print("exit: Please insert input!")
         exit()
     
-    
-    L = int(sys.argv[1])
-    
-    
-    '''
-    if(L % 3 !=0 or L<3 ): 
-        print("exit: Invalid input")
-        exit()
-    
-    '''
-    
-    
-    
-    main(L)  
-    # main(0, 1)
+    main()
