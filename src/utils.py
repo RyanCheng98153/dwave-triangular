1b46789711c3fa7bfdbd1d4deb25298234a00b3f
RyanCheng98153 <ryan98153@gmail.com>
2024-09-11 02:03:05 +0800

    refactor: centralize the utils function

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,23 @@
+import json
+
+def getfileData(filestr:str):
+    with open(filestr) as f:
+        data = f.readlines()
+        return data
+
+def getJson(filestr:str):
+    return json.loads( getfileData(filestr) )
+
+def getModelSize(file:str):
+    for i in range ( len(file) ):
+        if("model_size" in file[i]):
+            model_size = file[i+1].split()
+            return int(model_size[0]), int(model_size[2])
+
+def getAPItoken(filepath:str):
+    with open(filepath, 'r', encoding='utf-8') as F:
+        return F.readlines()
+        
 def toIndex(L, _i, _j) -> int:
     return L*_i + _j
 
