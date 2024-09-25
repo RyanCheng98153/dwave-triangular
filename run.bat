dffae4594d186dd79f3e7f5c842b521719050ed0
RyanCheng98153 <ryan98153@gmail.com>
2024-09-25 11:56:22 +0800

    exp: Experience for L 9~39 sample 1000

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
-set vars=21 24 27 30 33 36 39 42 45 48 51 54 57 60 129 255
-set nums=3 5 10 50 
+:: set vars=21 24 27 30 33 36 39 42 45 48 51 54 57 60 129 255
+set vars=9 12 15 18
+set nums=1000
 
 for %%n in (%nums%) do (
   for %%i in (%vars%) do (
